c837a17501ffa3ee923954a25ec930037964993f
Romano Zumbe <romano@blade.home.lan>
2025-10-13 21:04:49 +0200

    await run

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-    let _ = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = zero2prod::run(listener)
+        .expect("Failed to bind address")
+        .await;
     Ok(())
 }
