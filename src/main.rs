7c14667f5d61ace06135570ca80faca4b53791fd
Romano Zumbe <romano@workstation.home.lan>
2025-10-31 12:41:59 +0100

    Add postgres configuration to app_config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::net::TcpListener;
+use sqlx::{Connection, PgConnection};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string()).await
+        .expect("Failed to connect to postgres.");
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-    let _ = run(listener).expect("Failed to bind address").await;
+    let _ = run(listener, connection).expect("Failed to bind address").await;
     Ok(())
 }
